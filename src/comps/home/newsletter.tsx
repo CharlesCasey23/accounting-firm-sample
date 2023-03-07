a74e74c6c000585315a08eac8445debc57b8f3a8
CaseyMartin23 <cm232311@gmail.com>
2023-03-07 14:25:42 +0200

    shorted newsletter searchbar

--- src/comps/home/newsletter.tsx
+++ src/comps/home/newsletter.tsx
@@ -13,7 +13,7 @@ const Newsletter: React.FC = () => {
           Unique insights and information delivery to your inbox
         </div>
         <div className='flex flex-row mt-5 justify-center'>
-          <input className='p-2 border border-slate-400 md:mx-2 md:w-3/4' type='email' placeholder='Email Address' />
+          <input className='p-2 border border-slate-400 md:mx-2 md:w-2/6' type='email' placeholder='Email Address' />
           <button
             className={`py-2 px-3 text-lg md:text-xl ${headingRaleway.className} border-2 border-dark-blue bg-dark-blue text-white md:w-fit md:hover:bg-transparent md:hover:border-2 md:hover:border-dark-blue md:hover:text-dark-blue md:rounded`}
             onClick={() => console.log({ message: 'Button clicked!' })}
